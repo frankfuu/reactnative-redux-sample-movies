505c7f32aa5a9e42dc5bc8a8f44845bd2e54853b
Frank Fu <frankfu1122@gmail.com>
2022-08-28 18:07:16 +1000

    Add redux devtools to enable debugging redux state using React Native Debugger.

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,13 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import moviesReducer from './reducers';
+
+import { logger } from 'redux-logger'
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+
 const rootReducer = combineReducers({
   moviesReducer,
 });
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)));
+
